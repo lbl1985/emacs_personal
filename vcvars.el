e7e8f12aa52bf02bbb3e7bc4aa64bf68b17d51ad
Binlong Li <herbert19lee@gmail.com>
2015-09-09 04:02:17 -0700

    reformating the vcvar.el file

--- vcvars.el
+++ vcvars.el
@@ -1,20 +1,18 @@
 (setenv "PATH"
-	(concat
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow" ";"
-	 "C:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.0\\Framework\\v4.0\\" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VSTSDB\\Deploy" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\BIN" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\Tools" ";"
-	 "C:\\windows\\Microsoft.NET\\Framework\\v4.0.30319" ";"
-	 "C:\\windows\\Microsoft.NET\\Framework\\v3.5" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\VCPackages" ";"
-	 "C:\\Program Files (x86)\\HTML Help Workshop" ";"
-	 "C:\\Program Files (x86)\\HTML Help Workshop" ";"
-	 "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Team Tools\\Performance Tools" ";"
-	 "C:\\Program Files (x86)\\Windows Kits\\8.0\\bin\\x86" ";"
-	 "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v8.0A\\bin\\NETFX 4.0 Tools" ";"
-	 "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.0A\\bin\\" ";"
-	 (getenv "PATH")))
-
-
+    (concat
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow" ";"
+    "C:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.0\\Framework\\v4.0\\" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VSTSDB\\Deploy" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\BIN" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\Tools" ";"
+    "C:\\windows\\Microsoft.NET\\Framework\\v4.0.30319" ";"
+    "C:\\windows\\Microsoft.NET\\Framework\\v3.5" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\VCPackages" ";"
+    "C:\\Program Files (x86)\\HTML Help Workshop" ";"
+    "C:\\Program Files (x86)\\HTML Help Workshop" ";"
+    "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Team Tools\\Performance Tools" ";"
+    "C:\\Program Files (x86)\\Windows Kits\\8.0\\bin\\x86" ";"
+    "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v8.0A\\bin\\NETFX 4.0 Tools" ";"
+"C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.0A\\bin\\" ";"
+     (getenv "PATH")))
